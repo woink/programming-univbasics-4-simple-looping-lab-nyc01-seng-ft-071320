7b32b23c41fc788a9c3d761eccafb290bb8b8f9c
Ward Price <wardprice@gmail.com>
2020-06-27 14:47:37 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
-# Write your methods here
+def loop_message_five_times(string)
+  i = 0
+  while i <= 5 do
+    puts string
+    i += 1
+  end
+end
+
+def loop_message_n_times(message, integer)
+  i = 0 
+  while i <= integer do
+    puts message
+    i += 1 
+  end
+end
+
+def output_array(array)
+ i = 0 
+ while i < array.length do 
+   puts array[i]
+   i += 1 
+ end
+end
+
+def return_string_array(array)
+  i = 0 
+  new_array = []
+ while i < array.length do 
+  new_array.push(array[i].to_s)
+   i += 1 
+  end
+  new_array
+end
